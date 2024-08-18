9535fc398e813b2b284c708699b65078c8dfe61a
Damian X <dx4336969@gmail.com>
2024-08-18 11:10:30 +0000

    add react router

--- src/features/components/Button.jsx
+++ src/features/components/Button.jsx
@@ -1,7 +1,7 @@
 function Button({children}) {
     const base = 'bg-yellow-400'
     const styles ={
-        primary: base + ' p-2 px-4 uppercase text-base font-semibold rounded-full text-yellow-950 mt-4 hover:bg-yellow-300 transition-all'
+        primary: base + ' py-4 px-4 uppercase text-base font-semibold rounded-full text-yellow-950 mt-4 hover:bg-yellow-300 transition-all'
         
     }
     return (
