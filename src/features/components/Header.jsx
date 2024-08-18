446c4434791636e6bf08a7d1b786434f7baaa6fb
Damian X <dx4336969@gmail.com>
2024-08-18 06:30:32 +0000

    initial commit

---
+++ src/features/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <h1>Fast React Pizza Co.</h1>
+            <input type="search"  />
+            
+        </div>
+    )
+}
+
+export default Header
